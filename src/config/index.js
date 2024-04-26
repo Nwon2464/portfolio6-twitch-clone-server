8bbfb16e2452a1b7adfec03c397d33ed13a8e156
Namkyu Won <64752079+Nwon2464@users.noreply.github.com>
2024-04-26 20:40:19 +0900

    d
    d

--- src/config/index.js
+++ src/config/index.js
@@ -1,56 +1,56 @@
-const passport = require("passport");
-const chalk = require("chalk");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const Users = require("../models/user");
-// GOOGLE_CLIENT_ID="104901537063-ce1q2og53ifsa98lhhnued019v03o9sn.apps.googleusercontent.com"
-// GOOGLE_CLIENT_SECRET="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"
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+// const passport = require("passport");
+// const chalk = require("chalk");
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const Users = require("../models/user");
+// // GOOGLE_CLIENT_ID="104901537063-ce1q2og53ifsa98lhhnued019v03o9sn.apps.googleusercontent.com"
+// // GOOGLE_CLIENT_SECRET="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"
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
 
-// // // browser sends the cookie back and received the id
-passport.deserializeUser((id, done) => {
-  Users.findById(id).then((user) => {
-    done(null, user);
-  });
-});
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/redirect",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      // console.log(profile);
-      //   return null;
-      // console.log(profile);
-      try {
-        Users.findOne({
-          googleId: profile.id,
-        }).then((currentUser) => {
-          if (currentUser) {
-            //already have user
-            console.log("already have user");
-            done(null, currentUser);
-          } else {
-            // if not, create user in our db
-            new Users({
-              googleId: profile.id,
-              googleImage: profile._json.picture,
-              displayName: profile.displayName,
-              googleEmail: profile._json.email,
-            })
-              .save()
-              .then((newUser) => {
-                console.log("New data saved in MONGODB", newUser);
-                done(null, newUser);
-              });
-          }
-        });
-      } catch (error) {
-        throw new Error();
-      }
-    }
-  )
-);
+// // // // browser sends the cookie back and received the id
+// passport.deserializeUser((id, done) => {
+//   Users.findById(id).then((user) => {
+//     done(null, user);
+//   });
+// });
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: "/auth/google/redirect",
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       // console.log(profile);
+//       //   return null;
+//       // console.log(profile);
+//       try {
+//         Users.findOne({
+//           googleId: profile.id,
+//         }).then((currentUser) => {
+//           if (currentUser) {
+//             //already have user
+//             console.log("already have user");
+//             done(null, currentUser);
+//           } else {
+//             // if not, create user in our db
+//             new Users({
+//               googleId: profile.id,
+//               googleImage: profile._json.picture,
+//               displayName: profile.displayName,
+//               googleEmail: profile._json.email,
+//             })
+//               .save()
+//               .then((newUser) => {
+//                 console.log("New data saved in MONGODB", newUser);
+//                 done(null, newUser);
+//               });
+//           }
+//         });
+//       } catch (error) {
+//         throw new Error();
+//       }
+//     }
+//   )
+// );
